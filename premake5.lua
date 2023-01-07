8c70cad00bb32c67e2a0522a8441d0226bb0211e
PowerOfNames <PowerOfNames@web.de>
2023-01-07 18:14:43 +0100

    updated premake.lua and added bins to gitignore

--- premake5.lua
+++ premake5.lua
@@ -9,12 +9,20 @@ project "GLFW"
 	{
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
 		"src/context.c",
+		"src/egl_context.c",
+		"src/glx_context.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
+		"src/null_init.c",
+		"src/null_joystick.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/osmesa_context.c",
+		"src/platform.c",
 		"src/vulkan.c",
+		"src/wgl_context.c",
 		"src/window.c"
 	}
 
@@ -26,13 +34,11 @@ project "GLFW"
 		{
 			"src/win32_init.c",
 			"src/win32_joystick.c",
+			"src/win32_module.c",
 			"src/win32_monitor.c",
-			"src/win32_time.c",
 			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
+			"src/win32_time.c",
+			"src/win32_window.c"
 		}
 
 		defines 
